ac5e46723709665d237c5069d3bb62ca270633db
Gian-Luca Caluori <gianluca.caluori@students.fhnw.ch>
2019-12-18 13:25:15 +0100

    initial commit

---
+++ src/components/Car.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+//import { ReactComponent as Sign1 } from 'svg/401.svg';
+
+class Car extends React.Component {
+
+
+  render() {
+    return <div>
+      Car-Component
+    </div>;
+  }
+}
+
+export default Car;
